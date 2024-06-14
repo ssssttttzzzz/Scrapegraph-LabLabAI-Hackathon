60f13939aa1f82357b485c432d6b98ee47d1106c
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2024-06-14 08:53:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from helper import (
 from task import task
 from text_to_speech import text_to_speech
 
-st.set_page_config(page_title="search", page_icon=🔍")
+st.set_page_config(page_title="search", page_icon=✐")
 
 # Install playwright browsers
 playwright_install()
