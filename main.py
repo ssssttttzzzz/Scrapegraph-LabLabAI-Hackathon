3bddfe8185312117ee4e56e5cbaf149c8784a61e
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2024-06-14 17:15:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def save_email(email):
     with open("mails.txt", "a") as file:
         file.write(email + "\n")
 if "code" not in st.session_state:
-    st.session_state = 0
+    st.session_state['code'] = 0
 with st.sidebar:
     st.markdown("""---""")
     st.write("# Usage Examples")
