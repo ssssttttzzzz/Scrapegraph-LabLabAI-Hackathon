578f6e55f49f788b7c09980923920ab308caf0c6
ssssttttzzzz <47779363+ssssttttzzzz@users.noreply.github.com>
2024-06-14 11:18:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,18 @@ def save_email(email):
     with open("mails.txt", "a") as file:
         file.write(email + "\n")
 
-# with st.sidebar:
-#     st.markdown("""---""")
-#     st.write("# Usage Examples")
-#     st.write("## Prompt 1")
-#     st.write("- Give me all the news with their abstracts")
-#     st.write("## Prompt 2")
-#     st.write("- Create a voice summary of the webpage")
-#     st.write("## Prompt 3")
-#     st.write("- List me all the images with their visual description")
-#     st.write("## Prompt 4")
-#     st.write("- Read me the summary of the news")
-#     st.markdown("""---""")
+with st.sidebar:
+    st.markdown("""---""")
+    st.write("# Usage Examples")
+    st.write("## Prompt 1")
+    st.write("- Give me all the news with their abstracts")
+    st.write("## Prompt 2")
+    st.write("- Create a voice summary of the webpage")
+    st.write("## Prompt 3")
+    st.write("- List me all the images with their visual description")
+    st.write("## Prompt 4")
+    st.write("- Read me the summary of the news")
+    st.markdown("""---""")
 
 
 st.title("Search")
@@ -73,34 +73,3 @@ if st.button("Run the program", type="primary"):
             if graph_result:
                 add_download_options(graph_result)
 
-left_co2, *_, cent_co2, last_co2, last_c3 = st.columns([1] * 18)
-
-with cent_co2:
-    discord_link = "https://discord.com/invite/gkxQDAjfeX"
-    discord_logo = base64.b64encode(open("assets/discord.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{discord_link}" target="_blank">
-        <img src="data:image/png;base64,{discord_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
-
-with last_co2:
-    github_link = "https://github.com/VinciGit00/Scrapegraph-ai"
-    github_logo = base64.b64encode(open("assets/github.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{github_link}" target="_blank">
-        <img src="data:image/png;base64,{github_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
-
-with last_c3:
-    twitter_link = "https://twitter.com/scrapegraphai"
-    twitter_logo = base64.b64encode(open("assets/twitter.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{twitter_link}" target="_blank">
-        <img src="data:image/png;base64,{twitter_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
